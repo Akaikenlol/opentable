596e7b4b75c16de98bd91007c7c7c98cd98455c4
Akaikenlol <keithken972@gmail.com>
2023-08-13 00:10:31 +0630

    first commit

--- app/restaurant/[slug]/components/RestaurantNavBar.tsx
+++ app/restaurant/[slug]/components/RestaurantNavBar.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function RestaurantNavBar({ slug }: { slug: string }) {
 	return (
 		<nav className="flex text-reg pb-2 border-b">
-			<Link href={`restaurant/${slug}`} className="mr-4">
+			<Link href={`/restaurant/${slug}`} className="mr-4">
 				Overview
 			</Link>
 			<Link href="" className="mr-4">
 				Photo
 			</Link>
-			<Link href={`restaurant/${slug}/menu`} className="mr-4">
+			<Link href={`/restaurant/${slug}/menu`} className="mr-4">
 				Menu
 			</Link>
 			<Link href="" className="mr-4">
